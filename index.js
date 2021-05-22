9a47d12554f04bd51c489b84be60d3aa574f3466
ka <kabilanvennila@gmail.com>
2021-05-22 22:45:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+
+});
+
+router.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/about.html'));
+});
+
+router.get('/contact',function(req,res){
+  res.sendFile(path.join(__dirname+'/contact.html'));
+});
+
+router.get('/attributes',function(req,res){
+    res.sendFile(path.join(__dirname+'/attributes.html'));
+});
+
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
